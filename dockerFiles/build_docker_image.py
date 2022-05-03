cf1b18a1da68320e19a9a49f6cb73126c5cd32d5
Seyed-Ahmad Ahmadi <mangotee@gmail.com>
2022-05-03 21:37:11 +0200

    Initial commit.

---
+++ dockerFiles/build_docker_image.py
@@ -0,0 +1,24 @@
+import os
+
+# Do not forget to login to IEsegmap account first with the command
+# docker login
+
+# Specify version that will be appended to the Docker image name
+version = '0.1.0'
+
+# Get the current directory 
+current_folder = os.getcwd()
+
+# Download supplements
+os.system(f'wget https://www.dropbox.com/s/wdelnnxndgana0e/innerEarSupplements_v_0_1_3.zip?dl=0 -P {current_folder}')
+
+# Get the zip file 
+zip_file = os.path.join(current_folder, 'innerEarSupplements_v_0_1_3.zip?dl=0')
+
+# Unzip it and delete the zip file
+os.system(f'unzip {zip_file} -d {current_folder}')
+os.system(f'rm {zip_file}')
+
+# Docker build 
+docker_file = os.path.join(current_folder, 'Dockerfile')
+os.system(f'docker build -t iesegmap/ie-plane-normals:{version} {current_folder}/.')
